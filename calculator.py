b414c90241480d814afb9341c5eea31895a83faa
neha-dhananju <neha02dhananju@gmail.com>
2025-07-25 09:28:19 +0530

    Calculator buttons added

--- calculator.py
+++ calculator.py
@@ -10,16 +10,16 @@ root.resizable(False, False)  # Prevent resizing
 entry = tk.Entry(root, font=("Arial", 20), borderwidth=5, relief=tk.RIDGE, justify="right")
 entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10, ipady=10)
 
-btn_7=tk.entry(root, text="7", width=5, height=2, font=("Arial", 16))
+btn_7=tk.Button(root, text="7", width=5, height=2, font=("Arial", 16))
 btn_7.grid(row=1, column=0, padx=5, pady=5)
 
-btn_8=tk.entry(root , text="8" , width=5 , height=2 , font=('Arial',16))
+btn_8=tk.Button(root , text="8" , width=5 , height=2 , font=('Arial',16))
 btn_8.grid(row=1 , column=1 ,padx=5,pady=5)
 
-btn_9=tk.entry(root , text="9" , width=5 , height=2 , font=('Arial',16))
+btn_9=tk.Button(root , text="9" , width=5 , height=2 , font=('Arial',16))
 btn_9.grid(row=1 , column=2 ,padx=5,pady=5)
 
-btn_div=tk.entry(root , text="/" , width=5 , height=2 , font=('Arial',16))
+btn_div=tk.Button(root , text="/" , width=5 , height=2 , font=('Arial',16))
 btn_div.grid(row=1 , column=3 ,padx=5,pady=5)
 
 # Step 3: Run the main event loop
